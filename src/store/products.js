53b0f30d3aee184130299cb118ab07e13022e71a
Mohammed Galal <55014084+Mohammed-Galal@users.noreply.github.com>
2025-04-09 08:40:22 +0200

    Update products.js

--- src/store/products.js
+++ src/store/products.js
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import products from "./products.json";
 
+const prevCartItems = window.localStorage.getItem("cart") || [];
+
 const Products = {
     name: "products",
     initialState: {
       data: products,
-      cart: [],
+      cart: JSON.parse(prevCartItems),
       show: false,
     },
   },
@@ -24,21 +26,26 @@ reducers.addToCart = function (state, { data }) {
     };
     state.cart = [...state.cart, newItem];
   }
+
+  window.localStorage.setItem("cart", JSON.stringify(state.cart));
 };
 
 reducers.decreaseQuantity = function (state, { data }) {
   const targetItem = state.cart.find((i) => i.data.id === data.id);
   targetItem.quantity--;
   state.cart = state.cart.filter((i) => i.quantity > 0);
+  window.localStorage.setItem("cart", JSON.stringify(state.cart));
 };
 
 reducers.removeFromCart = function (state, { data }) {
   const targetItem = state.cart.find((i) => i.data.id === data.id);
   state.cart = state.cart.filter((i) => i.data.id !== targetItem.data.id);
+  window.localStorage.setItem("cart", JSON.stringify(state.cart));
 };
 
 reducers.clearCart = function (s) {
   s.cart = [];
+  window.localStorage.setItem("cart", JSON.stringify(s.cart));
 };
 
 reducers.showCart = function (state, { payload }) {
